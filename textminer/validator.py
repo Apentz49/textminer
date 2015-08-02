38191c12b68ca1fda9f9c7e9cca0787ded125ed1
Aaron Pentz <apentz49@gmail.com>
2015-08-02 14:47:07 -0700

    Added more to validator.py and test

--- textminer/validator.py
+++ textminer/validator.py
@@ -7,23 +7,30 @@ def binary_even(thing):
     return re.match(r"[01]+0$", thing)
 
 def hex(thing):
-    return re.match(r"[A-fa-f0-9]+$", thing)
-    # This is not passing,
+    match = re.match(r"^[A-Fa-f0-9]+$", thing)
 
+    if match:
+        return True
+    else:
+        return False
 
 def word(thing):
     return re.match(r"\A[\d]*[A-Za-z-]+\Z", thing)
     # \A = Start of string ignore m flag, \Z End ignore m flag
     # Not sure exactly what they do other then work
 
-def words():
+def words(thing):
     pass
 
 def phone_number(thing):
     return re.search(r"\(?(\d{3})\)?[\-\.]?\s*(\d{3})[\-\.]?(\d{4})", thing)
 
-def money():
-    pass
+def money(thing):
+    match = re.match(r"\$", thing)
+    if match:
+        return True
+    else:
+        return False
 
 def zipcode(thing):
     return re.match(r"^\d{5}(-\d{4})?$", thing)
